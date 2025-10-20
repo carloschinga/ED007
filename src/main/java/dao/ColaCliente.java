e1db766e74ad3b445a1ff4053b6c87f6aa7af6be
carloschinga <chinga_carlos@hotmail.com>
2025-10-20 16:41:41 -0500

    Implementacion de Cola

---
+++ src/main/java/dao/ColaCliente.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import com.sun.org.apache.xpath.internal.operations.Quo;
+import dto.Cliente;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *
+ * @author USER
+ */
+public class ColaCliente {
+    Queue<Cliente> colaCliente = new LinkedList<>();
+
+    public void agregar(Cliente c){
+        colaCliente.add(c);
+    }
+    public Cliente quitar(c);{
+        colaCliente.poll();
+    }
+    
+    public String imprimir(){
+    }
+    public static void main(String[] args) {
+        Cliente c1= new Cliente();
+        c1.setNombClie("Carlos");
+        Cliente c2= new Cliente();
+        c2.setNombClie("Juan");
+        
+        ColaCliente c= new ColaCliente();
+        c.agregar(c1);
+        c.agregar(c2);
+        System.out.println(c.imprimir();
+        c.quitar();
+        System.out.println(c.imprimir();
+    }
+    
+}
