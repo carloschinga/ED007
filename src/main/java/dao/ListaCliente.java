a62bea4c2384ad5506b07d898113ccec3102dd5e
carloschinga <chinga_carlos@hotmail.com>
2025-10-20 14:15:14 -0500

    Avance de ejemplo LinkedList

---
+++ src/main/java/dao/ListaCliente.java
@@ -0,0 +1,51 @@
+package dao;
+
+import dto.Cliente;
+import java.util.LinkedList;
+
+public class ListaCliente {
+    private LinkedList<Cliente> lista = new LinkedList<>();
+    private int apuntador=-1;
+    
+    public void agregar(Cliente c){
+        lista.add(c);    
+        //lista.addFirst(c); -> agregar al inicio
+        //lista.addLast(c); -> agregar al final
+        apuntador=lista.size()-1;
+    }
+    public Cliente getClienteActual(){
+        return lista.get(apuntador);
+    }
+    public Cliente inicio(){
+        apuntador=0;
+        return lista.getFirst();
+    }
+    public Cliente ultimo(){
+        apuntador=lista.size()-1;
+        return lista.getLast();
+    }
+    public Cliente siguiente(){
+        //Aqui debo programar como
+        // lo tengo en mi otros proyectos
+        //return lista.
+        return null;
+    }
+    public String imprimir(){
+        String resultado="";
+        for (Cliente cliente : lista) {
+            resultado+=cliente.getNombClie() +"\n";
+        }
+        return resultado;
+    }
+    
+    public static void main(String[] args) {
+        Cliente c1= new Cliente();
+        c1.setNombClie("Carlos");
+        Cliente c2= new Cliente();
+        c1.setNombClie("Juan");
+        ListaCliente l= new ListaCliente();
+        l.agregar(c1);
+        l.agregar(c2);
+        l.imprimir();
+    }
+}
---
+++ src/main/java/dto/Cliente.java
@@ -0,0 +1,14 @@
+package dto;
+
+import java.util.Date;
+
+
+public class Cliente {
+    private int codiClie;
+    private String appaClie;
+    private String apmaClie;
+    private String nombClie;
+    private double pesoClie;
+    private double tallClie;
+    private Date fechNaciClie;
+}
