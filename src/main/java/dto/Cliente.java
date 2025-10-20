a264c7dbe55a151cf3dabb9b8af5bd57ee4c23c8
Luxu <valladaresdiego006@gmail.com>
2025-10-20 14:50:11 -0500

    Solucion de error Valladares

--- src/main/java/dto/Cliente.java
+++ src/main/java/dto/Cliente.java
@@ -11,4 +11,78 @@ public class Cliente {
     private double pesoClie;
     private double tallClie;
     private Date fechNaciClie;
+
+    public Cliente(int codiClie, String appaClie, String apmaClie, String nombClie, double pesoClie, double tallClie, Date fechNaciClie) {
+        this.codiClie = codiClie;
+        this.appaClie = appaClie;
+        this.apmaClie = apmaClie;
+        this.nombClie = nombClie;
+        this.pesoClie = pesoClie;
+        this.tallClie = tallClie;
+        this.fechNaciClie = fechNaciClie;
+    }
+
+    
+    
+    public Cliente() {
+    }
+
+    public int getCodiClie() {
+        return codiClie;
+    }
+
+    public void setCodiClie(int codiClie) {
+        this.codiClie = codiClie;
+    }
+
+    public String getAppaClie() {
+        return appaClie;
+    }
+
+    public void setAppaClie(String appaClie) {
+        this.appaClie = appaClie;
+    }
+
+    public String getApmaClie() {
+        return apmaClie;
+    }
+
+    public void setApmaClie(String apmaClie) {
+        this.apmaClie = apmaClie;
+    }
+
+    public String getNombClie() {
+        return nombClie;
+    }
+
+    public void setNombClie(String nombClie) {
+        this.nombClie = nombClie;
+    }
+
+    public double getPesoClie() {
+        return pesoClie;
+    }
+
+    public void setPesoClie(double pesoClie) {
+        this.pesoClie = pesoClie;
+    }
+
+    public double getTallClie() {
+        return tallClie;
+    }
+
+    public void setTallClie(double tallClie) {
+        this.tallClie = tallClie;
+    }
+
+    public Date getFechNaciClie() {
+        return fechNaciClie;
+    }
+
+    public void setFechNaciClie(Date fechNaciClie) {
+        this.fechNaciClie = fechNaciClie;
+    }
+    
+    
+    
 }
