c979b7ebe9d2da84e3c0c474369de9f1596350fa
carloschinga <chinga_carlos@hotmail.com>
2025-10-20 15:31:39 -0500

    Lista enlazada

---
+++ src/main/java/dao/ListaSimpleCliente.java
@@ -0,0 +1,14 @@
+
+package dao;
+
+import dto.Cliente;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaSimpleCliente {
+    List<Cliente> lista= new ArrayList<>();
+    
+    public void agregar(){
+    
+    }
+}
