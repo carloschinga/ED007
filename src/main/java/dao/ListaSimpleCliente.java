d39d845f165da9dc31db466eabeb9529b206f7c0
carloschinga <chinga_carlos@hotmail.com>
2025-10-20 15:50:25 -0500

    Lista enlazada

--- src/main/java/dao/ListaSimpleCliente.java
+++ src/main/java/dao/ListaSimpleCliente.java
@@ -5,20 +5,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListaSimpleCliente {
-private int apuntador=-1;
+
+    private int apuntador = -1;
 
     List<Cliente> lista = new ArrayList<>();
+
     public void agregar(Cliente c) {
-        lista.add(c);    
-        apuntador=lista.size()-1;
+        lista.add(c);
+        apuntador = lista.size() - 1;
+    }
+
+    public String imprimir() {
+        String resultado = "";
+        for (Cliente c : lista) {
+            resultado += c.getNombClie() + "\n";
+        }
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        Cliente c1= new Cliente();
+        c1.setNombClie("Carlos");
+        Cliente c2= new Cliente();
+        c2.setNombClie("JUan");
+
+        ListaCliente lista= new ListaCliente();
+        lista.agregar(c1);
+        lista.agregar(c2);
+        System.out.println(lista.imprimir());
+        
     }
     
-    public String imprimir() {
-    String resultado = "";
-    for (Cliente c : lista) {
-        resultado += c.getNombClie() + "\n";
-    }
-    return resultado;
-}
-
 }
